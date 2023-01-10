3ff94632ffe9c00b8d15ad6c1c7dbdee583b3db3
CaioPedras <caio.vp@gmail.com>
2023-01-10 13:29:37 -0300

    result

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira um número: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            String row = "";
+            for (int j = 0; j < i; j++) {
+                row += i;
+            }
+            System.out.println(row);
+        }
     }
 }
